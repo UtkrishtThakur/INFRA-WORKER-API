91a97564ac85dac68923c4538699afbbeb5322ce
Utkrisht Thakur <utkrisht77880@gmail.com>
2026-01-15 01:26:30 +0530

    Redis client is better

--- redis_client.py
+++ redis_client.py
@@ -1,11 +1,7 @@
 import redis
 from config import settings
 
-
-# Create a Redis client using the connection URL
 redis_client = redis.Redis.from_url(
     settings.REDIS_URL,
     decode_responses=True,
-    ssl=True,
-    ssl_cert_reqs=None,
 )
