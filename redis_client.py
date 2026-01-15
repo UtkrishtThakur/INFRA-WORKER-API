f772fbda33bfbf1099b5d53904496984a026aee8
Utkrisht Thakur <utkrisht77880@gmail.com>
2026-01-15 11:24:35 +0530

    Redis updated

--- redis_client.py
+++ redis_client.py
@@ -4,6 +4,5 @@ from config import settings
 redis_client = redis.Redis.from_url(
     settings.REDIS_URL,
     decode_responses=True,
-    ssl=True,
-    ssl_cert_reqs=None,
+    ssl_cert_reqs=None,  # REQUIRED for Upstash
 )
