7dca4fd769783181abc4cab116917e2f2c202bd8
UtkrishtThakur <utkrisht77880@gmail.com>
2026-05-20 16:00:45 +0530

    Improved redis system

--- redis_client.py
+++ redis_client.py
@@ -1,8 +1,21 @@
+import logging
 import redis
 from config import settings
 
-redis_client = redis.Redis.from_url(
-    settings.REDIS_URL,
-    decode_responses=True,
-    ssl_cert_reqs=None,  # REQUIRED for Upstash
-)
+logger = logging.getLogger("securex.redis")
+
+is_ssl = settings.REDIS_URL.startswith("rediss://")
+
+if is_ssl:
+    logger.info("Initializing Redis client in SSL mode")
+    redis_client = redis.Redis.from_url(
+        settings.REDIS_URL,
+        decode_responses=True,
+        ssl_cert_reqs=None,  # REQUIRED for Upstash
+    )
+else:
+    logger.info("Initializing Redis client in normal (non-SSL) mode")
+    redis_client = redis.Redis.from_url(
+        settings.REDIS_URL,
+        decode_responses=True,
+    )
